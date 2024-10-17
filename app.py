f5344cde3d72f9122705070b226e5111d4dd863f
dndtn@DESKTOP-NIAI25F <dndtn6763@gamail.com>
2024-10-17 11:04:52 +0900

    updated

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def load_data():
     return df
 
 def main():
-    st.title("그냥")   
+    st.title("그냥", fontproperties=prop)   
     tips = load_data()
 
     # 데이터가공
@@ -20,9 +20,9 @@ def main():
     # 시각화 차트
     fig, ax = plt.subplots(ncols=2, figsize=(10, 6), sharex=True, sharey=True)
     ax[0].scatter(x = m_tips['total_bill'], y = m_tips['tip'])
-    ax[0].set_title('남성')
+    ax[0].set_title('남성', fontproperties=prop)
     ax[1].scatter(x = f_tips['total_bill'], y = f_tips['tip'])
-    ax[1].set_title('여성')
+    ax[1].set_title('여성', fontproperties=prop)
 
     # 중요포인트
     # plt.show()
